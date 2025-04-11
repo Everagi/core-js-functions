a848460750387c5ead5e18c218f7b7c0faff7809
Everagi <belorusskiychuvak@gmail.com>
2025-04-11 11:30:20 +0300

    feat: solve the tasks generator func and partial arguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -205,8 +205,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function inner(...args2) {
+    const allArgs = [...args1, ...args2];
+    return fn(...allArgs);
+  };
 }
 
 /**
@@ -226,8 +229,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom;
+  return function innerId() {
+    const result = currentId;
+    currentId += 1;
+    return result;
+  };
 }
 
 module.exports = {
